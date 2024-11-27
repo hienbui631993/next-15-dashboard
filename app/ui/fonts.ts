84136dab83b77472b460182c864a4bceaec00d4f
hienbui631993 <hienbui631993@gmail.com>
2024-11-27 17:48:41 +0700

    display: 'swap'

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,3 @@
 import {Inter, Lusitana} from "next/font/google";
 export const GF_Inter_Font = Inter({subsets:['latin']});
-export const lusitana = Lusitana({subsets:['latin'],weight:"700"});
+export const lusitana = Lusitana({subsets:['latin'],display: 'swap',weight:"700"});
